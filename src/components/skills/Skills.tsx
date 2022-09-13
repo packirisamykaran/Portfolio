1ed6acf6242d0c51e168172377386a3706ec15c2
packirisamykaran <43819446+packirisamykaran@users.noreply.github.com>
2022-09-13 21:59:00 +0800

    skills update

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,16 +4,19 @@ import "../style/Skills.css"
 export default function Skills() {
 
   const techs:any = {
-    "JavaScript" : "https://img.icons8.com/color/344/javascript--v1.png",
+   
     "TypeScript" : "https://img.icons8.com/fluency/344/typescript.png",
     "ReactJS":"https://img.icons8.com/color/344/react-native.png",
     "NodeJS" :"https://img.icons8.com/color/344/nodejs.png",
+    "NextJS":"https://img.icons8.com/color/344/nextjs.png",
     "ExpressJS": "https://img.icons8.com/ios-filled/344/javascript.png",
     "Java":"https://img.icons8.com/color/344/java-coffee-cup-logo--v1.png",
+    "Python":"https://img.icons8.com/fluency/344/python.png",
     "MongoDB": "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/344/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png",
     "SQL" : "https://img.icons8.com/external-soft-fill-juicy-fish/344/external-sql-coding-and-development-soft-fill-soft-fill-juicy-fish.png",
     "Firebase": "https://img.icons8.com/color/344/firebase.png",
-    "Sass":"https://img.icons8.com/color/344/sass.png"
+    "Sass":"https://img.icons8.com/color/344/sass.png",
+    "Rust":"https://cdn.icon-icons.com/icons2/2699/PNG/512/rust_lang_logo_icon_169776.png"
   }
 
   const techDisplay = Object.keys(techs).map((tech:string, index)=>{
